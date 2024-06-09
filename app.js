5e7b445a64ea60f7d0302c651c78a6ce98f6fcb0
Gsrcordeiro <cordeirogb@hotmail.com>
2024-06-09 17:56:06 -0300

    novoapp

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const os = require("os");
+const app = express();
+
+app.get('/liveness', (req, res) => {
+    return res
+    .status(200)
+    .json({
+        message: "Meu APP está Vivo!",
+        path: process.cwd(),
+        gid: process.getgid(),
+        uid: process.getuid(),
+    })
+});
+
+
+app.get('/readiness', (req, res) => {
+    return res
+        .status(500)
+        .json({
+            message: "Meu APP está Vivo!",
+            plataform: os.platform(),
+            freemem: os.freemem(),
+            homedir: os.homedir(),
+            date: new Date().getTime(),
+        })
+});
+
+module.exports = app;
